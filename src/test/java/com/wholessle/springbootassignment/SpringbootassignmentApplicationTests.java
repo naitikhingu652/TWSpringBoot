911822f1356cb4e84755aaaceede1907cc5e674b
unknown <naitikhingu652@gmail.com>
2018-04-27 12:23:36 +0530

    initial commit

---
+++ src/test/java/com/wholessle/springbootassignment/SpringbootassignmentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wholessle.springbootassignment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootassignmentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
