3ed4294955af9f4ac204bd4ae3e567969ab7def6
unknown <naitikhingu652@gmail.com>
2018-05-03 05:11:21 +0530

    Incomplete code for Make One Array

---
+++ src/main/java/com/wholessle/springbootassignment/controller/MakeOneArrayController.java
@@ -0,0 +1,19 @@
+package com.wholessle.springbootassignment.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wholessle.springbootassignment.service.MakeOneArrayService;
+
+/**
+ * Created by Naitik Hingu on Apr 27, 2018
+ *
+ **/
+@RestController
+public class MakeOneArrayController {
+	
+	@Autowired MakeOneArrayService makeOneArrayService;
+	
+	//TODO
+
+}
---
+++ src/main/java/com/wholessle/springbootassignment/service/MakeOneArrayServiceImpl.java
@@ -0,0 +1,14 @@
+package com.wholessle.springbootassignment.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Naitik Hingu on Apr 27, 2018
+ *
+ **/
+@Service
+public class MakeOneArrayServiceImpl implements MakeOneArrayService {
+	
+	//TODO
+
+}
